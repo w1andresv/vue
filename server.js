48f532782865ef61e388fcd7748491d579e21d40
weymar <w1andresv@gmail.com>
2021-05-12 09:17:27 -0500

    ajuste deploy-2

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist'));
+app.use(express.static('./dist/seguros-web'));
 
 app.get('/*', function(req,res) {
 
-    res.sendFile(path.join(__dirname,'/dist/index.html'));
+    res.sendFile(path.join(__dirname,'/dist/seguros-web/index.html'));
 });
 
 // Start the app by listening on the default Heroku ports
